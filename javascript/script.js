15388a3b6682954ff48d36e838e35a7831ac83d4
DavideZattra <d.zattra@gmail.com>
2021-08-06 14:38:22 +0200

    created the step to complete the exercise

---
+++ javascript/script.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * 
+ * 
+ * A - Mail
+ *     Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere 
+ *     (che creerete arbitrariamente voi), e stampa un messaggio appropriato sull’esito del controllo.
+ *     
+ * B - Gioco dei dadi
+ *     Generare un numero random da 1 a 6, sia per il giocatore o giocatrice sia per il computer. 
+ *     Stabilire il vincitore o la vincitrice, in base a chi fa il punteggio più alto.
+ */
+
+// A
+// Create the array with the registered e-mail
+// Get the user e-mail with a prompt
+// Check if the user email is in the e-mail array
+// Create the appropiate message 
+
+// B 
+// Establish the variables
+// Generate randomic number for the user
+// Generate randomic number for the computer
+// Compare the assigned numbers to get the winner
